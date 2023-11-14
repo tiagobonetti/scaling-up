b0b74e0d64f98f1dd53b3a7caed95a636a257d74
Joep Vanlier <joep.vanlier@gmail.com>
2023-11-15 00:44:14 +0100

    godot has no structured bindings ;_;

--- Scripts/catapult.gd
+++ Scripts/catapult.gd
@@ -1,6 +1,7 @@
 extends Area2D
 
-var product = preload("res://Scenes/Entities/Product.tscn")
+var LaunchArc = preload("res://Scripts/launch_arc.gd")
+var Product = preload("res://Scenes/Entities/Product.tscn")
 var reticle_color = Color(1.0, 0.0, 0.0)
 var shadow_color = Color(0.0, 0.0, 0.0)
 var start_position = Vector2()
@@ -8,10 +9,14 @@ var current_position = Vector2()
 var is_dragging = false
 var redraw = false
 var debug_drawing = true
+var perspective_factor = 0.6
 
 
 func _to_screen_coordinates(x):
 	return global_transform.affine_inverse() * x
+
+func faux_3d(xy, z):
+	return xy - Vector2(0, perspective_factor * z)
 
 
 # Called when the node enters the scene tree for the first time.
@@ -25,43 +30,29 @@ func _process(_delta):
 
 
 func _draw():
-	if is_dragging:		
-		var perspective_factor = 0.8
-		var v0 = 0.2 * (current_position - start_position).length()  # Velocity is determined by drag length
-		v0 = max(v0, 0.01)  # Prevent div by zero
-		var gravity = 9.81
-		var theta = 2.0 * PI * 72 / 360
-		var ground_angle = atan2(current_position[1] - start_position[1], current_position[0] - start_position[0])
-		var total_time = 2.0 * v0 * sin(theta) / gravity
+	if is_dragging:
+		var arc = LaunchArc.new(	
+			start_position,
+			0.2 * (current_position - start_position).length(),  # Velocity is determined by drag length
+			atan2(current_position[1] - start_position[1], current_position[0] - start_position[0])
+		)
 		
-		var xc = start_position[0]
-		var yc = start_position[1]
-		var zc = 0
-		var num_segments = 25.0
-		var dt = total_time / (num_segments - 1)
-		for step in range(num_segments):
-			var t = dt * step
-			var xc_last = xc
-			var yc_last = yc
-			var zc_last = zc
-			xc = start_position[0] - v0 * t * cos(theta) * cos(ground_angle)
-			yc = start_position[1] - v0 * t * cos(theta) * sin(ground_angle)
-			zc = v0 * t * sin(theta) - 0.5 * gravity * t * t
-			
+		var xy = start_position
+		var z = 0
+
+		for coordinate in arc.trajectory(25):
 			draw_line(
-				_to_screen_coordinates(Vector2(xc_last, yc_last - perspective_factor * zc_last)),
-				_to_screen_coordinates(Vector2(xc, yc - perspective_factor * zc)),
+				_to_screen_coordinates(faux_3d(xy, z)),
+				_to_screen_coordinates(faux_3d(coordinate[0], coordinate[1])),
 				reticle_color,
 			)
+			xy = coordinate[0]
+			z = coordinate[1]
 		
+		var final_position = arc.final_position()
 		draw_line(
 			_to_screen_coordinates(start_position),
-			_to_screen_coordinates(
-				Vector2(
-					start_position[0] - v0 * total_time * cos(theta) * cos(ground_angle),
-					start_position[1] - v0 * total_time * cos(theta) * sin(ground_angle)
-					)
-				),
+			_to_screen_coordinates(faux_3d(final_position[0], final_position[1])),
 			shadow_color
 		)
 		redraw = false
@@ -84,13 +75,11 @@ func _input(event):
 		is_dragging = false
 		redraw = debug_drawing
 		
-		var launched_product = product.instantiate()
+		var launched_product = Product.instantiate()
 		add_child(launched_product)
 		launched_product.position = _to_screen_coordinates(start_position)
-		print(launched_product)
-		launched_product.apply_impulse(- 5 * (current_position - start_position))
-		
-	
+		# launched_product.apply_impulse(- 5 * (current_position - start_position))
+
 	if is_dragging and event is InputEventMouseMotion:
 		current_position = event.position
 		redraw = debug_drawing
--- Scripts/launch_arc.gd
+++ Scripts/launch_arc.gd
@@ -1,5 +1,3 @@
-
-
 # Constraints
 # => Flight should be parabolic.
 # => Start and endpoint are given.
@@ -17,33 +15,61 @@
 # (r1 - r0) * g = v0 * v0 * sin(2 * theta)
 # v0 = sqrt((r1 - r0) * g / sin(2 * theta))
 
-class LaunchArc:
-	# Maybe gravity should be some global state?
-	func _init(start_position, v0, ground_angle, theta=deg_to_rad(72), gravity=9.81):
-		self.v0 = max(v0, 0.01)  # Prevent div by zero
-		self.total_time = 2.0 * v0 * sin(theta) / gravity
-		self.start_position = start_position
-		self.direction = Vector2(cos(ground_angle), sin(ground_angle))
-		self.theta = theta
-		self.gravity = gravity
+var v0: float
+var total_time: float
+var start_position: Vector2
+var direction: Vector2
+var theta: float
+var gravity: float
+
+
+# Maybe gravity should be some global state?
+func _init(start_position, v0, ground_angle, theta=deg_to_rad(72), gravity=9.81):
+	self.v0 = max(v0, 0.01)  # Prevent div by zero
+	self.total_time = 2.0 * v0 * sin(theta) / gravity
+	self.start_position = start_position
+	self.direction = Vector2(cos(ground_angle), sin(ground_angle))
+	self.theta = theta
+	self.gravity = gravity
+
+func target_position(from_position, to_position, theta=deg_to_rad(72), gravity=9.81):
+	var v0 = sqrt((to_position - from_position) * gravity / sin(2 * theta))
+	var ground_angle = atan2(to_position[1] - from_position[1], to_position[0] - from_position[0])
 	
-	func target_position(from_position, to_position, theta=deg_to_rad(72), gravity=9.81):
-		var v0 = sqrt((to_position - from_position) * gravity / sin(2 * theta))
-		var ground_angle = atan2(to_position[1] - from_position[1], to_position[0] - from_position[0])
-		
-		# Barf. This is how you make an instance of this class apparently.
-		return get_script().new(from_position, v0, ground_angle, theta, gravity)
-		
-		#var v0 = 0.2 * (current_position - start_position).length()  # Velocity is determined by drag length
+	# Barf. This is how you make an instance of this class apparently.
+	return get_script().new(from_position, v0, ground_angle, theta, gravity)
+	
+func position_at(time):
+	return [
+		self.start_position - self.v0 * time * cos(self.theta) * self.direction,
+		self.v0 * time * sin(self.theta) - 0.5 * self.gravity * time * time,
+	]
 
-	func position_at(time):
-		return [
-			self.start_position - self.v0 * time * cos(self.theta) * self.direction,
-			self.v0 * time * sin(self.theta) - 0.5 * self.gravity * time * time,
-		]
+func final_position():
+	return position_at(self.total_time)
 
-	func step_arc(num_segments):
-		var dt = self.total_time / (num_segments - 1)
-		for step in range(num_segments):
-			return self.position_at(dt * step)
-			# oh no, godot has no yield :(
+func trajectory(num_steps):
+	return Iterator.new(self, num_steps)
+
+
+class Iterator:
+	var _parent
+	var _step: int = 0
+	var _num_segments:int = 0
+	var _dt: float
+	
+	func _init(parent, num_steps):
+		self._parent = parent
+		self._num_segments = num_steps
+		self._dt = self._parent.total_time / (self._num_segments - 1)
+	
+	func _iter_init(_arg):
+		self._step = 0
+		return self._step < self._num_segments
+
+	func _iter_next(_arg):
+		self._step += 1
+		return self._step < self._num_segments
+
+	func _iter_get(_arg):
+		return self._parent.position_at(self._dt * self._step)
